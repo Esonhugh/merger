3a930ce670c17c455aa878b083d74c80e7dfca2b
Esonhugh <esonhughoutside@gmail.com>
2023-05-14 22:39:07 +0800

    add up docker file

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM golang AS builder
+LABEL authors="esonhugh"
+
+WORKDIR /build
+
+ADD go.mod .
+COPY . .
+RUN go build -o doc_merger cmd/main/main.go
+
+
+FROM golang
+
+WORKDIR /
+
+COPY --from=builder /build/test/ /test/
+COPY --from=builder /build/init.sh /init.sh
+COPY --from=builder /build/doc_merger /doc_merger
+
+CMD ["/init.sh"]
---
+++ init.sh
@@ -0,0 +1,13 @@
+./doc_merger add -f ./test/MergeTwoMoreDocument/oneforall_output -t csv  \
+  domain subdomain \
+  source source
+
+./doc_merger add -f ./test/MergeTwoMoreDocument/subfinder_output -t json  \
+  domain host \
+  source source
+
+./doc_merger merge -o out.data \
+  -s 'struct { Name   string `select:"domain"`; Source string `select:"source"`}' \
+  -d=false
+
+cat out.data
